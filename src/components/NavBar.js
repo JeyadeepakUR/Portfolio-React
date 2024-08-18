34165ff865649b692564a3ddc3ac498dbe73b8ce
JeyadeepakUR <jeyadeepak2005@gmail.com>
2024-08-18 20:05:28 +0530

    resume change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
-import resume from '../JeyadeepakURResume.pdf'
+import resume from './JeyadeepakURResume.pdf'
 
 export const NavBar = () => {
 
